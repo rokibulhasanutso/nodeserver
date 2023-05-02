39880e6de5d58ad8eca552e6fdcf6ad47960b223
Rokibul Hasan Utso <mynetlify88@gmail.com>
2023-05-03 00:41:50 +0600

    design 404 content

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const port = 3000;
-const publicdir = path.join(__dirname, 'public');
+const publicdir = path.join(__dirname, 'Public');
 
 const tempDirs = {
   '/': { file: 'index.html', type: 'text/html' },
@@ -12,7 +12,7 @@ const tempDirs = {
   '/style.css': { file: 'style.css', type: 'text/css' }
 };
 
-const server = http.createServer((req, res) => {
+http.createServer((req, res) => {
   const tempDir = tempDirs[req.url];
   if (tempDir) {
     fs.readFile(path.join(publicdir, tempDir.file), 'utf-8', (err, data) => {
@@ -23,10 +23,26 @@ const server = http.createServer((req, res) => {
     });
   } else {
     res.writeHead(404, { 'Content-Type': 'text/html' });
-    res.end('<h1>404, Not Found This page.</h1>')
-  }
-});
-
-server.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+    res.end(`<div style="
+        width: 100%;
+        height: 100vh;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        color: gray;
+        font-family: sans-serif;
+        font-weight: 500;
+        flex-direction: column;
+        margin-top: -50px;
+    ">
+      <h1 style="
+        font-weight: 500;
+        font-size: 50px;
+        letter-spacing: 18px;
+        margin-bottom: 0;
+      ">404</h1>
+      <h2 style="
+          font-weight: 500;
+      ">This page is Not Found.</h2></div>`)
+    }
+}).listen(port, () => console.log(`Server listening on port ${port}`));
